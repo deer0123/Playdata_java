15db146742234ae973f48ef3c453b82741ea5e24
deer0123 <cheol0123@gmail.com>
2024-08-12 00:44:32 +0900

    Initial commit

---
+++ src/basic/Comment.java
@@ -0,0 +1,26 @@
+package basic;
+
+/**
+ *   API Document 주석 : 웹사이트에 올렸을 때
+ *   자동으로 html태그로 변환되어 코드를 보지 않아도
+ *   웹에 나타나는 주석
+ *
+ * @author - Hong
+ * @email - honggildong@gmail.com
+ */
+
+// 한 줄 주석(라인 주석) 입니다.
+// 자바는 클래스라는 블록에서 코드가 시작됩니다.
+public class Comment {
+
+    //함수를 호출해서 실행하려면 main함수가 필요합니다.
+    public static void main(String[] args) {
+        /*
+						블록 주석입니다.
+						블록 내의 모든 텍스트가 주석으로 처리됩니다.
+            메인 함수는 프로그램의 실행 시작 지점이며
+            메인 함수 없이 프로그램은 실행되지 않습니다.
+        */
+        System.out.println("hello");
+    }
+}
---
+++ src/basic/Variable.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class Variable {
+    int age = 27;
+    int Age = 29; //둘은 서로 다른 변수
+
+    //int 700 = 365; (x) 숫자로만 x
+    //int 7number = 7; (x) 숫자로 시작해도 x
+    int number7 = 7; // ok
+    int num7ber = 7; // ok
+
+    //int my birth day = 19921013; 공백 x
+    int myBirthDay = 19921013; //ok
+
+    // String class = "클래스";  키워드 x
+    String Class = "클래스"; // 추천 x
+    String className = "클래스"; // 차라리 이렇게.
+
+    int i = 3; // 별 뜻 없다면 상관없지만, 중요한 값이라면 뜻이 있게 작성하기.
+}
